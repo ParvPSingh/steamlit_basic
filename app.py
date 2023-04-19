328d74e0fc83bfb4b6ff6a45a1ebcfa136ec5175
ParvPSingh <96364716+ParvPSingh@users.noreply.github.com>
2023-04-19 23:08:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import streamlit as st
 def find_max(a,b,c):
   l=list([a,b,c])
   return max(l)
-a = st.number_input()
-b = st.number_input()
-c = st.number_input()
-res = find_max(a,b,c)
+a = st.number_input("First number", min_value=0, step=1)
+b = st.number_input("Second number", min_value=0, step=1)
+c = st.number_input("Third number", min_value=0, step=1)
+
 if st.button("Largest No"):
+  res = find_max(a,b,c)
   st.write(res)
